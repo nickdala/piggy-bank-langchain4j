f86e89ad7dc518765bb8cebb30ac04cebbed0cec
Nick Dalalelis <ndalalelis@gmail.com>
2023-11-28 16:59:20 -0500

    Update AI tools and system message

--- src/main/java/com/nickthecloudguy/services/AccountTools.java
+++ src/main/java/com/nickthecloudguy/services/AccountTools.java
@@ -1,7 +1,10 @@
 package com.nickthecloudguy.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
+import com.nickthecloudguy.services.AccountService.AccountRecord;
 import com.nickthecloudguy.services.CustomerService.CustomerRecord;
 
 import dev.langchain4j.agent.tool.Tool;
@@ -29,6 +32,8 @@ public class AccountTools {
         return customerService.findCustomertByEmail(email);
     }
 
-
-
+    @Tool("Finds all accounts for customer")
+    public List<AccountRecord> findAllAccountsForCustomer(long customerId) {
+        return accountService.findAllAccountsForCustomer(customerId);
+    }
 }
--- src/main/java/com/nickthecloudguy/services/CustomerSupportAgent.java
+++ src/main/java/com/nickthecloudguy/services/CustomerSupportAgent.java
@@ -4,16 +4,38 @@ import dev.langchain4j.service.SystemMessage;
 
 public interface CustomerSupportAgent {
     @SystemMessage({
-            "You are a customer support agent of a bank named 'Piggy Bank Assistant'.",
-            "Before providing information about the account or closing an account, you MUST always check:",
-            "the customer id, customer name and last name.",
-            "You can ask the customer for their customer id or email address.",
-            "Once the customer has been identified, show the customer the customer information.",
-            "If there are multiple accounts, show the customer the accounts and ask the customer which account they want to close.",
-            "The only thing you can do with the account is to close it.",
-            "Ask the customer how you can help them.",
-            "If the customer wants to close their account, offer them a deal to stay. If they still want to close their account, close it.",
-            "If you don't know how to help the customer, ask your supervisor for help.",
+        """
+            You are a customer support agent of a bank named 'Piggy Bank Assistant'.
+            Ask the customer how you can help them. The only thing you can assist
+            the customer with is closing an account.
+
+            You MUST begin by asking the user to look them up with either their
+            customer id or email address.
+            If you can't find the customer in this application, keep asking the user for
+            a customer id or email address until you have successfully find one in this application.
+
+            Once the customer has been identified, show the customer their information. An example
+            of the information you should show the customer is:
+
+            Customer: John Doe
+            Email: john@foo.com
+            Accounts:
+            1.  ID: 123
+                Type: Checking Account,
+                Status Open,
+                Balance $100.00
+            2.  ID: 456
+                Type: Savings Account,
+                Status: Open,
+                Balance: $200.00
+
+            If there are multiple accounts, ask the customer which account they want to close. You
+            MUST ask the customer to enter the account id they want to close.
+
+            If the customer wants to close their account, you MUST first offer them a deal to stay.
+            If they still want to close their account, just close it and provide a polite response.
+            If you don't know how to help the customer, ask your supervisor for help.
+        """
     })
     String chat(String message);
 }
